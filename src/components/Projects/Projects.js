93889ffed8e70a8f90dcecc408c6f9c7a72a376c
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-10-02 11:26:59 -0300

    una card en proyecto

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,18 +10,11 @@ function Projects({ darkMode }) {
       repoUrl: 'https://github.com/beta-cancri/SalvandoHuellitas-Front-end',
       liveUrl: 'https://salvandohuellitas-front-end-production-5e5d.up.railway.app/',
     },
-    {/*{
-      name: 'Otro Proyecto',
-      description: 'Breve descripción proyecto. 🚀',
-      imageUrl: 'url-de-imagen',
-      repoUrl: 'https://github.com/tu-repo',
-      liveUrl: 'https://url-de-app',
-    }*/}
   ];
 
   return (
     <section id="projects" className={`projects-section ${darkMode ? 'dark-mode' : ''}`}>
-      <h2>Proyectos </h2>
+      <h2>Proyectos</h2>
       <div className="projects-grid">
         {projects.map((project, index) => (
           <div key={index} className={`project-card ${darkMode ? 'dark-mode' : ''}`}>
@@ -29,10 +22,9 @@ function Projects({ darkMode }) {
             <h3>{project.name}</h3>
             <p>{project.description}</p>
             <div className="project-links">
-            <a href={project.repoUrl} target="_blank" rel="noopener noreferrer" className="project-link">🔗 Ver Código</a>
-            {/*<a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="project-link">🌐 Ver en Vivo</a>*/}
-</div>
-
+              <a href={project.repoUrl} target="_blank" rel="noopener noreferrer" className="project-link">🔗 Ver Código</a>
+              <a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="project-link">🌐 Ver en Vivo</a>
+            </div>
           </div>
         ))}
       </div>
@@ -41,4 +33,3 @@ function Projects({ darkMode }) {
 }
 
 export default Projects;
-
