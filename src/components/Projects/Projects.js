128d3652fa660299f4a30572786afaf622919156
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-11-27 15:05:08 -0300

    links icons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -55,8 +55,9 @@ function Projects({ darkMode }) {
             <h3>{project.name}</h3>
             <p>{project.description}</p>
             <div className="project-links">
-              <a href={project.repoUrl} target="_blank" rel="noopener noreferrer" className="project-link">🔗 Ver Código</a>
-              <a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="project-link">🌐 Ver en Vivo</a>
+              <a href={project.repoUrl} target="_blank" rel="noopener noreferrer" className="project-link"> <i className="fab fa-github" style={{ marginRight: '5px' }}></i> </a>
+              <a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="project-link">
+              <i className="fas fa-globe" style={{ marginRight: '5px' }}></i></a>
             </div>
           </div>
         ))}
